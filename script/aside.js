cf3ce77ef3c497e4c1aeb605550abda2fde005dc
StellLegend <jiinhuynh@gmail.com>
2019-05-14 23:12:38 +0700

    finish detail page, aside javascript

--- script/aside.js
+++ script/aside.js
@@ -1,12 +1,13 @@
 
 
 var items = document.getElementsByClassName("aside__list-item")
-var active = document.getElementsByClassName("active")
+var actived = document.getElementsByClassName("active")
 for(var i=0; i<items.length; i++){
   items[i].onclick = function(){
     var list = document.getElementById("aside__list");
     // if (!active) {
         list.insertBefore(this, list.childNodes[0]);
+        actived[0].style.display = "none"
     //}
     //list.style.overflow = "hidden";
   }
